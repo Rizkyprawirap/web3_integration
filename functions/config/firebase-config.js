2bbdef8bb85eecab9f956e573d3664c9854c3545
rizkyprawirap <rizkyprawirap@gmail.com>
2022-07-21 14:18:10 +0700

    Update for privatekey

--- functions/config/firebase-config.js
+++ functions/config/firebase-config.js
@@ -1,6 +1,6 @@
 const { initializeApp, cert} = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('./web3-integration.json');
+const serviceAccount = require('./web3-storage-key.json');
 
 initializeApp({
     credential: cert(serviceAccount),
